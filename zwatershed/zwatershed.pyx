72f6c9905f963ee85091efe0f941464adb7f1e0d
Jan Funke <jfunke@iri.upc.edu>
2016-10-10 15:18:50 +0200

    prettied up zwatershed_unified
    • documentation
    • ensure passed numpy arrays are contiguous (and do nothing if they are)
    • removed code duplicate

--- zwatershed/zwatershed.pyx
+++ zwatershed/zwatershed.pyx
@@ -9,40 +9,81 @@ import os
 cimport numpy as np
 import h5py
 
-def zwatershed_unified(np.ndarray[np.float32_t, ndim=4] affs, threshes, np.ndarray[uint32_t, ndim=3] gt = None):
+def zwatershed_unified(np.ndarray[np.float32_t, ndim=4] affs, thresholds, np.ndarray[uint32_t, ndim=3] gt = None):
+    '''
+    Compute segmentations from an affinity graph for several thresholds.
+
+    Passed volumes need to be converted into contiguous memory arrays. This will
+    be done for you if needed, but you can save memory by making sure your
+    volumes are already C_CONTIGUOUS.
+
+    Parameters
+    ----------
+
+        affs: numpy array, float32, 4 dimensional
+
+            The affinities as an array with affs[channel][z][y][x].
+
+        thresholds: list of int
+
+            The thresholds to compute segmentations for. For each threshold, one
+            segmentation is returned.
+
+        gt  : numpy array, uint32, 3 dimensional (optional)
+
+            An optional ground-truth segmentation as an array with gt[z][y][x].
+            If given, metrics
+
+    Returns
+    -------
+
+        segmentations
+
+            List of segmentations (numpy arrays, uint64, 3 dimensional).
+
+        (segmentations, metrics)
+
+            Tuple of segmentations and metrics, if ground-truth volume was
+            given.
+    '''
 
     cdef vector[uint64_t*]            segmentation_data
     cdef np.ndarray[uint64_t, ndim=3] segmentation
-    cdef uint32_t*                    gt_data
+    cdef uint32_t*                    gt_data = NULL
+
+    # the C++ part assumes contiguous memory, make sure we have it (and do 
+    # nothing, if we do)
+    if not affs.flags['C_CONTIGUOUS']:
+        print("Creating memory-contiguous affinity arrray (avoid this by passing C_CONTIGUOUS arrays)")
+        affs = np.ascontiguousarray(affs)
+    if gt is not None and not gt.flags['C_CONTIGUOUS']:
+        print("Creating memory-contiguous ground-truth arrray (avoid this by passing C_CONTIGUOUS arrays)")
+        gt = np.ascontiguousarray(gt)
 
     print("Preparing segmentation volumes...")
 
     segmentations = []
     volume_shape = (affs.shape[1], affs.shape[2], affs.shape[3])
-    threshes.sort()
-    for i in range(len(threshes)):
+    thresholds.sort()
+    for i in range(len(thresholds)):
         segmentation = np.zeros(volume_shape, dtype=np.uint64)
         segmentations.append(segmentation)
         segmentation_data.push_back(&segmentation[0,0,0])
 
+    if gt is not None:
+        gt_data = &gt[0,0,0]
+
     print("Processing thresholds")
 
-    if gt is not None:
-
-        gt_data = &gt[0,0,0]
-        metrics = process_thresholds(
-            threshes,
-            affs.shape[1], affs.shape[2], affs.shape[3],
-            &affs[0, 0, 0, 0],
-            segmentation_data,
-            gt_data)
-        return (segmentations, metrics)
-
-    process_thresholds(
-        threshes,
+    metrics = process_thresholds(
+        thresholds,
         affs.shape[1], affs.shape[2], affs.shape[3],
         &affs[0, 0, 0, 0],
-        segmentation_data)
+        segmentation_data,
+        gt_data)
+
+    if gt is not None:
+        return (segmentations, metrics)
     return segmentations
 
 #-------------- interface methods --------------------------------------------------------------
@@ -303,7 +344,6 @@ cdef extern from "zwatershed.h":
         double rand_merge
 
     vector[Metrics] process_thresholds(vector[size_t] thresholds, size_t width, size_t height, size_t depth, np.float32_t* affs, vector[uint64_t*]& segmentation_data, uint32_t* gt_data)
-    vector[Metrics] process_thresholds(vector[size_t] thresholds, size_t width, size_t height, size_t depth, np.float32_t* affs, vector[uint64_t*]& segmentation_data)
 
     #struct RegionGraphEdge {
 
